0afedb54ecee2393ddbbac3058c41ba145b55cfa
Kristen Peck <kpeck71@gmail.com>
2017-09-19 03:06:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 def key_for_min_value(name_hash)
   #name_hash.collect {|item, num| item }.sort_by
-  name_hash.collect do |item, num|
+  name_hash.collect do |key, value|
       if num > num + 1
         num, num + 1 = num + 1, num
       end
