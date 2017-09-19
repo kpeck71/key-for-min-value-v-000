70eaeda6090c313d2475c29a4ef14c1606c64267
Kristen Peck <kpeck71@gmail.com>
2017-09-19 03:02:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,7 @@
 def key_for_min_value(name_hash)
   #name_hash.collect {|item, num| item }.sort_by
   name_hash.collect do |item, num|
-    swapped = false
-    name_hash.each do |item, num|
-      if name_hash[num] > name_hash[num + 1]
-        name_hash[num], name_hash[num + 1] = name_hash[num + 1], name_hash[num]
-        swapped = true
-      end
-    end
-    break if not swapped
+    name_hash.each |item, num|
+  item.first
   end
 end
